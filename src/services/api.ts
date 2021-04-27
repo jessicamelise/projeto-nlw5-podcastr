2c4382c83e4615c570c4d129fe11a4fc97ee276f
jessicamelise <jessica.melise@hotmail.com>
2021-04-26 21:07:51 -0300

    ajuste url vercel

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,12 +4,11 @@
 //   baseURL: 'http://localhost:3333/',
 // });
 
-
 export const api = async () => {
   const url =
     process.env.NODE_ENV === "development"
       ? "http://localhost:3000/server.json"
-      : `projeto-nlw5-podcastr-4u05et710-jessicamelise.vercel.app/server.json`
+      : `https://projeto-nlw5-podcastr-4u05et710-jessicamelise.vercel.app/server.json`
 
   const response = await fetch(url);
   const data = await response.json();
