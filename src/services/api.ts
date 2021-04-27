105aacf6058f0c766abd7cb54d558ac8d09b6687
jessicamelise <jessica.melise@hotmail.com>
2021-04-26 21:54:39 -0300

    ajuste url para vercel

--- src/services/api.ts
+++ src/services/api.ts
@@ -8,7 +8,7 @@ export const api = async () => {
   const url =
     process.env.NODE_ENV === "development"
       ? "http://localhost:3000/server.json"
-      : `${process.env.VERCEL_URL}/server.json`
+      : `${process.env.NEXT_PUBLIC_VERCEL_URL}/server.json`
 
   const response = await fetch(url);
   const data = await response.json();
